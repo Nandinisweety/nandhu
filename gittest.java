c885dd2d2575a643ecfe4f18e3f18773e3abbf93
Nandinisweety <103484987+Nandinisweety@users.noreply.github.com>
2022-04-12 12:05:45 +0530

    Add files via upload

---
+++ gittest.java
@@ -0,0 +1,10 @@
+public class MyFirstJavaProgram {
+
+   /* This is my first java program.
+    * This will print 'nandini' as the output
+    */
+
+   public static void main(String []args) {
+      System.out.println("Hello nandini"); // prints nandini
+   }
+}
